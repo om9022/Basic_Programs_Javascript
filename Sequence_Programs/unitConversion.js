553b188552beca045117149b07ca3919aa257ddb
om9022 <omkarchikane607@gmail.com>
2021-08-11 18:08:01 +0530

    Added : Program of Unit Conversion

---
+++ Sequence_Programs/unitConversion.js
@@ -0,0 +1,10 @@
+// 1ft = 12 in then 42 in = ? ft
+console.log("The 42 inch ="+ 42/12);
+
+// Rectangular Plot of 60 feet x 40 feet in meters
+let inMeter = (60 * 40) * 0.092903
+console.log("Area of rectangular plot is " + inMeter)
+
+// Calculate area of 25 such plots in acres
+let AreaOf25Plots = inMeter * 25 
+console.log("Area of 25 plots in acre are :" + AreaOf25Plots * 0.000247105);
