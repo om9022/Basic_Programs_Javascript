606f543fde04d684689438857b1df66f1a247f69
om9022 <omkarchikane607@gmail.com>
2021-08-11 18:06:15 +0530

    Added : Program to check sum and average

---
+++ Sequence_Programs/SumAndAverage.js
@@ -0,0 +1,9 @@
+num1 = (Math.floor(Math.random() * 100) % 90) + 10
+num2 = (Math.floor(Math.random() * 100) % 90) + 10
+num3 = (Math.floor(Math.random() * 100) % 90) + 10
+num4 = (Math.floor(Math.random() * 100) % 90) + 10
+num5 = (Math.floor(Math.random() * 100) % 90) + 10
+
+let sum = num1 + num2 + num3 + num4 + num5
+let avg = sum/5
+console.log("sum of numbers:" +sum+ "average is :"+avg);
