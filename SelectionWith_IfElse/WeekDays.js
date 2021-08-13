7c03386630142500816667551a1ade0c566b32cd
om9022 <omkarchikane607@gmail.com>
2021-08-13 17:38:57 +0530

    Added: Program to print Days

---
+++ SelectionWith_IfElse/WeekDays.js
@@ -0,0 +1,23 @@
+prompt = require("prompt-sync")();
+let Day = prompt("Enter a Day number")
+if(Day == 1){
+    console.log("Sunday")
+}
+else if(Day == 2){
+    console.log("Monday")
+}
+else if(Day == 3){
+    console.log("Tuesday")
+}
+else if(Day == 4){
+    console.log("Wednesday")
+}
+else if(Day == 5){
+    console.log("Thursday")
+}
+else if(Day == 6){
+    console.log("Friday")
+}
+else if(Day == 7){
+    console.log("Saturday")
+}
