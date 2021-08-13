084c8745ccffd24f1f8f4eb21af2b914086df6ef
om9022 <omkarchikane607@gmail.com>
2021-08-13 17:33:41 +0530

    Added: Program to display Numbers in Words

---
+++ SelectionWith_IfElse/WriteSingleDigitNumber.js
@@ -0,0 +1,32 @@
+prompt = require("prompt-sync")();
+let Number = prompt("Enter a number")
+if(Number == 1){
+    console.log("One")
+}
+else if(Number == 2){
+    console.log("Two")
+}
+else if(Number == 3){
+    console.log("Three")
+}
+else if(Number == 4){
+    console.log("Four")
+}
+else if(Number == 5){
+    console.log("Five")
+}
+else if(Number == 6){
+    console.log("Six")
+}
+else if(Number == 7){
+    console.log("Seven")
+}
+else if(Number == 8){
+    console.log("Eight")
+}
+else if(Number == 9){
+    console.log("nine")
+}
+else if(Number == 0){
+    console.log("Zero")
+}
