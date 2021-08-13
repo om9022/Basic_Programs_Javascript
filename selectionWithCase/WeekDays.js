24167575b763bbd3a08c9d682e405b9829898b1c
om9022 <omkarchikane607@gmail.com>
2021-08-13 18:43:27 +0530

    Added : Program to print week Days with case

---
+++ selectionWithCase/WeekDays.js
@@ -0,0 +1,32 @@
+prompt = require("prompt-sync")();
+let number = prompt("Enter a number to print Day : ")
+
+switch (number) {
+    case "1":
+        console.log("Sunday")
+        break;
+
+    case "2":
+        console.log("Monday")
+        break;
+
+    case "3":
+        console.log("Tuesday")
+        break;
+
+    case "4":
+        console.log("Wednesday")
+        break;
+
+    case "5":
+        console.log("Thursday")
+        break;
+
+    case "6":
+        console.log("Friday")
+        break;
+
+    case "7":
+        console.log("Saturday")
+        break;
+}
